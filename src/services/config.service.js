814891eac1df0067b421e428a5e326f385be5738
Joshua Rainbow <joshua-rainbow@resulting-it.com>
2022-09-21 15:56:17 +0100

    test REPLACE

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'https://quizapixyz-env.eba-rne8mazt.eu-west-2.elasticbeanstalk.com/';
+    service.apiBase = '';
 
     return service;
 }]);
